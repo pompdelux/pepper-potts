f4354e145ca08ee24569d59ff92e038abb77a332
ulrik nielsen <ulrik@o3.dk>
2014-12-18 14:55:36 +0100

    10min

--- jobs/ordercount2.rb
+++ jobs/ordercount2.rb
@@ -7,16 +7,16 @@ config = config[:hanzo]
 SCHEDULER.every '10m', :first_in => 0 do |job|
   # response = JSON.parse(Net::HTTP.get(URI(config[:dsn] + "?action=ordercount")))
   targets = [
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*AT.count)%2C%221h%22)%2C%22.AT%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*CH.count)%2C%221h%22)%2C%22.CH%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.COM.count)%2C%221h%22)%2C%22COM%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*DE.count)%2C%221h%22)%2C%22.DE%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*DK.count)%2C%221h%22)%2C%22.DK%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*FI.count)%2C%221h%22)%2C%22.FI%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*NL.count)%2C%221h%22)%2C%22.NL%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*NO.count)%2C%221h%22)%2C%22.NO%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*SE.count)%2C%221h%22)%2C%22.SE%22)",
-    "target=alias(summarize(sumSeries(stats.counters.prod.order.*.count)%2C%221h%22)%2C%22ALL%22)"
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*AT.count)%2C%2210min%22)%2C%22.AT%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*CH.count)%2C%2210min%22)%2C%22.CH%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.COM.count)%2C%2210min%22)%2C%22COM%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*DE.count)%2C%2210min%22)%2C%22.DE%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*DK.count)%2C%2210min%22)%2C%22.DK%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*FI.count)%2C%2210min%22)%2C%22.FI%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*NL.count)%2C%2210min%22)%2C%22.NL%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*NO.count)%2C%2210min%22)%2C%22.NO%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*SE.count)%2C%2210min%22)%2C%22.SE%22)",
+    "target=alias(summarize(sumSeries(stats.counters.prod.order.*.count)%2C%2210min%22)%2C%22ALL%22)"
   ].join('&')
 
   response = JSON.parse(Net::HTTP.get(URI("http://pdlstats1.pompdelux.com/render/?_salt=1418900351.465&from=-1hours&"+targets+"&format=json")))
